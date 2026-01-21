80eae5990bde0732d4691d148ae3584825dd0b27
naims <naimdrive6@gmail.com>
2026-01-21 22:43:06 +0600

    jwt implement

--- src/actions/server/auth.ts
+++ src/actions/server/auth.ts
@@ -6,8 +6,6 @@ import { User } from "@/src/model/user-model";
 import { collections, dbConnect } from "@/src/lib/dbConnect";
 import { LoginFormData } from "@/src/types/auth/LoginFormData";
 
-
-
 // method 1
 export const postUser = async (payload: SignupFormData) => {
   const { email, password } = payload;
@@ -42,7 +40,9 @@ export const postUser = async (payload: SignupFormData) => {
 export const loginUser = async (payload: LoginFormData) => {
   const { email, password } = payload;
 
-  const user = await dbConnect(collections.USERS).findOne({ email: email.toLowerCase() });
+  const user = await dbConnect(collections.USERS).findOne({
+    email: email.toLowerCase(),
+  });
 
   if (!user) {
     return null;
@@ -67,7 +67,7 @@ export const loginUser = async (payload: LoginFormData) => {
 export const createUser = async (userData: SignupFormData) => {
   try {
     await User.create(userData);
-  } catch  {
+  } catch {
     throw new Error("Failed to create user");
   }
 };
